be6a29d8deaa34a0ef4055ff33628716558d37c4
Tamas Sagi <sagi.tamas93@gmail.com>
2025-06-29 22:12:18 +0200

    feat: :sparkles: example usage added

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import logging
+import time
+
+from polarrrgh.logger.logger import MProcHandlerCtx, close, get_logger
+
+
 def main():
-    print("Hello from polarrrgh!")
+    logger.debug("debug")
+    logger.info("info")
+
+    x = 8 / 0
+    logger.warning("warning")
+    logger.error("error")
+    logger.critical("critical")
 
 
 if __name__ == "__main__":
-    main()
+    with MProcHandlerCtx(mproc=True) as logger:
+        main()
