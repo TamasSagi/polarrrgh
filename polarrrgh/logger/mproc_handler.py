8313ef8f075860c745617cec2026e4b2fe1c3fb1
Tamas Sagi <sagi.tamas93@gmail.com>
2025-06-29 22:12:08 +0200

    feat: :sparkles: MProcHandler added

--- polarrrgh/logger/mproc_handler.py
+++ polarrrgh/logger/mproc_handler.py
@@ -1,23 +1,76 @@
-import json
+import logging
 import multiprocessing
 import pickle
+import queue
 import threading
-from logging import Handler, LogRecord
-from pathlib import Path
-from typing import Any
+import traceback
+from io import StringIO
+
+from polarrrgh.logger.config import LoggerConfig
+from polarrrgh.logger.handler import Handler
 
 
 class MProcHandler(Handler):
     """
-    This class handles logging from multiple processes using a shared queue.
+    Handles logging from multiple processes using a shared queue.
     """
 
-    def __init__(self):
-        Handler.__init__(self)
+    SENTINEL = "BYE"
 
-        config = MProcHandler._read_default_config()
+    def __init__(self, config: LoggerConfig):
+        super().__init__(config)
 
-    def _read_default_config(
-        path: Path = Path(__file__).parent / "config" / "default.json",
-    ) -> dict[str, Any]:
-        json.load(path.open(encoding="utf-8"))
+        self.manager = multiprocessing.Manager()
+        self.queue = self.manager.Queue()
+
+        self.receiver = threading.Thread(target=self.receive, daemon=False)
+        self.receiver.start()
+
+    @staticmethod
+    def _format_record(record: logging.LogRecord) -> logging.LogRecord:
+        record.msg = record.msg % record.args
+        record.args = None
+        record.exc_info = None
+
+        return record
+
+    def receive(self) -> None:
+        while True:
+            try:
+                record: logging.LogRecord = self.queue.get()
+
+                if record == MProcHandler.SENTINEL:
+                    break
+
+                for handler in self.handlers:
+                    if handler.level <= record.levelno:
+                        handler.emit(record)
+
+            except (EOFError, KeyboardInterrupt):
+                break
+
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            record = MProcHandler._format_record(record)
+            self.queue.put_nowait(record)
+        except (ValueError, RuntimeError, queue.Full):
+            self.handleError(record)
+
+    def close(self):
+        self.queue.put(MProcHandler.SENTINEL)
+        self.receiver.join()
+
+        for handler in self.handlers:
+            try:
+                handler.acquire()
+                handler.flush()
+                handler.close()
+
+            except (OSError, ValueError):
+                pass
+
+            finally:
+                handler.release()
+
+        self.manager.shutdown()
+        self.handlers.clear()
